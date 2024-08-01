7324344b0eb16b0663d28c9bb7cf972b5245910a
Zed <51093497+robersonrodrigo@users.noreply.github.com>
2024-08-01 16:56:07 -0400

    Update hlestlock-analyzer.sh

--- hlestlock-analyzer.sh
+++ hlestlock-analyzer.sh
@@ -1,199 +1,170 @@
 #!/bin/bash
 
-# Localização do arquivo de log do Apache
-LOG_FILE="/var/log/apache2/access.log"
+# Define cores para formatação
+cor_vermelho="\e[1;31m"   # Define vermelho claro e negrito
+cor_verde="\e[1;32m"     # Define verde claro e negrito
+cor_reset="\e[0m"        # Reseta a formatação para o padrão
 
-# Função para imprimir o banner
-print_banner() {
-  echo -e "\033[31m"
-  echo -e "                                                         "
-  echo -e "   _   _   _           _     _            _              "
-  echo -e "  | | | | | |         | |   | |          | |             "
-  echo -e "  | |_| | | | ___  ___| |_  | | ___   ___| |_            "
-  echo -e "  |  _  | | |/ _ \/ __| __| | |/ _ \ / __| __|           "
-  echo -e "  | | | | | |  __/ \__ \ |_ | | (_) | (__| |_            "
-  echo -e "  \_| |_/ |_|\___| |___/\__||_|\___/ \___|\__|  v1.0.0   "
-  echo -e "                                                         "
-  echo -e "                   Log Analyzer                          "
-  echo -e "                   Developed by ZeD                      "
-  echo -e "                                                         "
-  echo -e "\033[0m"
+# Função para exibir o banner de uso
+banner() {
+    echo "Uso: ./nome.sh  /caminho/do/arquivo.log"
 }
 
-# Função para imprimir separador
-print_separator() {
-  echo "------------------------------------------------------------"
+# Função para exibir o banner principal
+banner2() {
+    echo -e "\033[31m"
+    echo -e ""
+    echo -e "   _   _  _           _     _            _              "
+    echo -e "  | | | || |         | |   | |          | |             "
+    echo -e "  | |_| || | ___  ___| |_  | | ___   ___| |_            "
+    echo -e "  |  _  || |/ _ \/ __| __| | |/ _ \ / __| __|           "
+    echo -e "  | | | || |  __/ \__ \ |_ | | (_) | (__| |_            "
+    echo -e "  \_| |_/|_|\___| |___/\__||_|\___/ \___|\__|  v1.0.0   "
+    echo -e "                                                        "
+    echo -e "                  Log Analyzer                          "
+    echo -e "                  Developed by ZeD                      "
+    echo ""   
+    echo -e "\033[0m"
 }
 
-# Função para verificar se o arquivo de log existe
-check_log_file() {
-  if [[ ! -f $LOG_FILE ]]; then
-    echo "Arquivo de log não encontrado!"
-    exit 1
-  fi
+# Verifica se o arquivo de log é válido e tem permissão de leitura
+validar_arquivo() {
+    if [[ -r "$1" && "$1" == *.log ]]; then
+        return 0
+    else
+        echo "Arquivo de log inválido ou sem permissão de leitura"
+        banner
+        exit 1
+    fi
 }
 
-# Função para análise de IPs mais frequentes
-analyze_top_ips() {
-  clear
-  echo "Top 10 IPs que acessaram o servidor:"
-  awk '{print $1}' $LOG_FILE | sort | uniq -c | sort -nr | head -n 10
-  print_separator
+# Gera um array contendo os IPs únicos
+gerar_array_ips() {
+    mapfile -t ips < <(cut -d " " -f 1 "$1" | sort -u)
 }
 
-# Função para análise de URLs mais acessadas
-analyze_top_urls() {
-  clear
-  echo "Top 10 URLs mais acessadas:"
-  awk '{print $7}' $LOG_FILE | sort | uniq -c | sort -nr | head -n 10
-  print_separator
+# Gera arquivos de requisições por IP
+gerar_requisicoes_por_ip() {
+    for ip in "${ips[@]}"; do
+        grep "$ip" "$1" > "${ip}.txt"
+    done
 }
 
-# Função para análise de agentes de usuário mais comuns
-analyze_user_agents() {
-  clear
-  echo "Top 10 Agentes de Usuário (Browsers):"
-  awk -F\" '{print $6}' $LOG_FILE | sort | uniq -c | sort -nr | head -n 10
-  print_separator
+# Limpa arquivos temporários
+limpar_arquivos_temporarios() {
+    rm -f *.txt
 }
 
-# Função para análise de erros mais comuns
-analyze_common_errors() {
-  clear
-  echo "Erros mais comuns:"
-  awk '{print $9}' $LOG_FILE | grep "^ *[45]" | sort | uniq -c | sort -nr | head -n 10
-  print_separator
+# Listar IPs e quantidades de acesso
+listar_ips() {
+    clear 
+    banner2
+    echo -e "${cor_vermelho}IPs que acessam o servidor${cor_reset}"
+    echo -e "${cor_verde}Qtd de acessos:${cor_reset}"
+    cut -d " " -f 1 "$1" | sort | uniq -c | sort -unr
 }
 
-# Função para análise de picos de tráfego por hora
-analyze_traffic_peaks() {
-  clear
-  echo "Picos de tráfego por hora:"
-  awk '{print $4}' $LOG_FILE | cut -d: -f2 | sort | uniq -c | sort -nr | head -n 10
-  print_separator
+# Mostrar quantidade de acessos por recurso
+acessos_por_recurso() {
+    clear
+    banner2
+    echo -e "${cor_vermelho}Quantidade de acessos por recurso${cor_reset}"
+    for ip in "${ips[@]}"; do
+        echo -e "${cor_verde}Recursos mais acessados pelo IP: $ip${cor_reset}"
+        grep "$ip" "$1" | cut -d " " -f 7 | sort | uniq -c
+    done
 }
 
-# Função para análise de códigos de status HTTP
-analyze_http_status() {
-  clear
-  echo "Distribuição dos códigos de status HTTP:"
-  awk '{print $9}' $LOG_FILE | sort | uniq -c | sort -nr
-  print_separator
+# Mostrar User-Agents utilizados por IP
+user_agents_por_ip() {
+    clear
+    banner2
+    for ip in "${ips[@]}"; do
+        echo -e "${cor_verde}User-Agents utilizados pelo IP: $ip${cor_reset}"
+        grep "$ip" "$1" | cut -d '"' -f 6-10 | cut -d '"' -f 1 | sort | uniq -c
+    done
 }
 
-# Função para identificar possíveis tentativas de SQL Injection
-analyze_sql_injection() {
-  clear
-  echo "Possíveis tentativas de SQL Injection:"
-  grep -Ei "union.*select.*|\'.*or.*1=1" $LOG_FILE
-  print_separator
+# Identificar ferramentas utilizadas pelos IPs
+ferramentas_por_ip() {
+    clear
+    banner2
+    for ip in "${ips[@]}"; do
+        echo -e "${cor_vermelho}Ferramentas utilizadas pelo IP: $ip${cor_reset}"
+        grep "$ip" "$1" | cut -d '"' -f 6-10 | egrep -oi "AppleWebKit|nmap|curl|wget|nikto" | sort -u
+    done
 }
 
-# Função para identificar possíveis tentativas de RFI
-analyze_rfi() {
-  clear
-  echo "Possíveis tentativas de Remote File Inclusion (RFI):"
-  grep -Ei "((http|https):\/\/|ftp:\/\/)" $LOG_FILE
-  print_separator
+# Procurar acessos por arquivo
+procurar_acesso_por_arquivo() {
+    clear
+    banner2
+    echo -e "${cor_vermelho}Qual arquivo deseja procurar?${cor_reset}"
+    echo -e "${cor_verde}Ex: passwords.txt${cor_reset}"
+    read -r arquivo
+    grep -i "$arquivo" "$1" | sort -u
 }
 
-# Função para identificar possíveis tentativas de LFI
-analyze_lfi() {
-  clear
-  echo "Possíveis tentativas de Local File Inclusion (LFI):"
-  grep -Ei "(\/\.\.\/|\/etc\/passwd)" $LOG_FILE
-  print_separator
-}
-
-# Função para identificar possíveis tentativas de scanners de vulnerabilidade
-analyze_scanners() {
-  clear
-  echo "Possíveis tentativas de Scanners de Vulnerabilidade (Nikto, Nmap, etc.):"
-  grep -Ei "nikto|nmap|acunetix|w3af|nessus" $LOG_FILE
-  print_separator
-}
-
-# Função para identificar possíveis tentativas de brute force
-analyze_brute_force() {
-  clear
-  echo "Possíveis tentativas de Brute Force:"
-  awk '{print $1}' $LOG_FILE | sort | uniq -c | sort -nr | awk '$1 > 100'
-  print_separator
-}
-
-# Função para análise de tráfego por método HTTP
-analyze_http_methods() {
-  clear
-  echo "Distribuição de métodos HTTP:"
-  awk '{print $6}' $LOG_FILE | cut -d'"' -f2 | sort | uniq -c | sort -nr
-  print_separator
-}
-
-# Função para contagem de bytes transferidos
-analyze_byte_transfer() {
-  clear
-  echo "Total de bytes transferidos por IP:"
-  awk '{print $1, $10}' $LOG_FILE | awk '{arr[$1]+=$2} END {for (i in arr) print arr[i], i}' | sort -nr | head -n 10
-  print_separator
+# Mostrar primeiro e último acesso por IP
+primeiro_ultimo_acesso() {
+    clear
+    banner2
+    for ip in "${ips[@]}"; do
+        echo -e "${cor_verde}Primeiro acesso do IP: $ip${cor_reset}"
+        grep "$ip" "$1" | head -n1
+        echo -e "${cor_verde}Último acesso do IP: $ip${cor_reset}"
+        grep "$ip" "$1" | tail -n1
+    done
 }
 
 # Função para exibir o menu
-show_menu() {
-  echo "Selecione uma opção:"
-  echo ""
-  echo " 1) Top 10 IPs que acessaram o servidor"
-  echo " 2) Top 10 URLs mais acessadas"
-  echo " 3) Top 10 Agentes de Usuário (Browsers)"
-  echo " 4) Erros mais comuns"
-  echo " 5) Picos de tráfego por hora"
-  echo " 6) Distribuição dos códigos de status HTTP"
-  echo " 7) Possíveis tentativas de SQL Injection"
-  echo " 8) Possíveis tentativas de Remote File Inclusion (RFI)"
-  echo " 9) Possíveis tentativas de Local File Inclusion (LFI)"
-  echo "10) Possíveis tentativas de Scanners de Vulnerabilidade"
-  echo "11) Possíveis tentativas de Brute Force"
-  echo "12) Distribuição de métodos HTTP"
-  echo "13) Total de bytes transferidos por IP"
-  echo "14) Sair"
-  echo ""
+exibir_menu() {
+    echo ""
+    echo -e "${cor_verde}Menu:${cor_reset}"
+    echo -e "[1] - Listar IPs"
+    echo -e "[2] - Quantidade de acessos por recurso"
+    echo -e "[3] - User-Agents utilizados por IP"
+    echo -e "[4] - Ferramentas utilizadas pelos IPs"
+    echo -e "[5] - Procurar acesso por arquivo"
+    echo -e "[6] - Primeiro/Último acesso por IP"
+    echo -e "[7] - Sair"
+    echo ""
 }
 
 # Função principal
 main() {
-  # Imprimir o banner
-  print_banner
+    if [ "$#" -ne 1 ]; then
+        banner
+        exit 1
+    fi
 
-  # Verificar se o arquivo de log existe
-  check_log_file
+    validar_arquivo "$1"
+    gerar_array_ips "$1"
+    gerar_requisicoes_por_ip "$1"
 
-  # Loop principal para exibir o menu e processar a escolha do usuário
-  while true; do
-    # Exibir o menu
-    show_menu
-
-    # Ler a escolha do usuário
-    read -p "Digite a sua escolha [1-14]: " choice
-
-    # Processar a escolha do usuário
-    case $choice in
-      1) analyze_top_ips ;;
-      2) analyze_top_urls ;;
-      3) analyze_user_agents ;;
-      4) analyze_common_errors ;;
-      5) analyze_traffic_peaks ;;
-      6) analyze_http_status ;;
-      7) analyze_sql_injection ;;
-      8) analyze_rfi ;;
-      9) analyze_lfi ;;
-      10) analyze_scanners ;;
-      11) analyze_brute_force ;;
-      12) analyze_http_methods ;;
-      13) analyze_byte_transfer ;;
-      14) echo "Saindo..." ; exit 0 ;;
-      *) echo "Opção inválida. Por favor, tente novamente." ;;
-    esac
-  done
+    while true; do
+        exibir_menu
+        echo -e "${cor_verde}Escolha uma opção:${cor_reset}"
+        read -r opcao
+        case "$opcao" in
+            1) listar_ips "$1" ;;
+            2) acessos_por_recurso "$1" ;;
+            3) user_agents_por_ip "$1" ;;
+            4) ferramentas_por_ip "$1" ;;
+            5) procurar_acesso_por_arquivo "$1" ;;
+            6) primeiro_ultimo_acesso "$1" ;;
+            7) 
+                echo "Saindo..."
+                limpar_arquivos_temporarios
+                exit 0
+                ;;
+            *) 
+                echo "Opção inválida, tente novamente."
+                ;;
+        esac
+        echo ""
+    done
 }
 
-# Executar a função principal
-main
+banner2
+main "$@"
